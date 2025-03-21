970d8a153eb441ab41b47f058e168e71f46c14dc
davi-IF <dmnm2@aluno.ifal.edu.br>
2025-03-21 12:29:16 -0300

    change

--- Calculo_Viagem.py
+++ Calculo_Viagem.py
@@ -2,4 +2,4 @@ time = float(input("type the time (hours):"))
 distance = float(input("type the distance(km):"))
 
 vm = distance/time
-print(f"Total VM: {vm:5.2f}km/h")                       
+print(f"Total VM:{vm:5.2f}km/h")
