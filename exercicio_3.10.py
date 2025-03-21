7d68c885beb9644a804b6a7cfbbdd12681d298e3
Davi_Matheus <166820985+Davi-IF@users.noreply.github.com>
2025-03-21 11:06:02 -0300

    new

---
+++ exercicio_3.10.py
@@ -0,0 +1,19 @@
+dias = int(input("type a value of days :"))
+hours = int(input("type a value of hours:"))
+min = int(input("type a value of min:"))
+sec = int(input("type the seconds:"))
+
+
+convert_dias = dias * 24 * 60 * 60
+convert_hours = hours * 60 * 60
+convert_min =  min * 60
+result = (convert_dias+convert_hours+convert_min+sec)
+
+
+""""
+convert_hours = (convert_day + hours) * 60
+convert_min = (convert_hours + min) * 60
+convert_sec = convert_min + sec
+result_sec = convert_seecee """
+
+print(result)
---
+++ testemath1.py
@@ -0,0 +1,13 @@
+salario = 1500
+aumento = 0.05
+novo_sal = salario + salario*aumento
+
+print("seu novo salario:" ,novo_sal)
+
+
+# --------------------------------------------------
+
+print(salario +(salario*(5/100)))
+
+
+#---------------------------------------------------
